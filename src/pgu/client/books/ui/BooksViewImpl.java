8657695cbd5b5a5749bb194cc4f4a9f8d84b28f7
Guilcher Pascal <pgu.dev@gmail.com>
2012-07-12 21:44:03 +0200

    ..

--- src/pgu/client/books/ui/BooksViewImpl.java
+++ src/pgu/client/books/ui/BooksViewImpl.java
@@ -1,14 +1,21 @@
 package pgu.client.books.ui;
 
+import java.util.ArrayList;
+
 import pgu.client.books.BooksView;
+import pgu.shared.domain.Book;
 
 import com.github.gwtbootstrap.client.ui.Button;
+import com.github.gwtbootstrap.client.ui.Column;
+import com.github.gwtbootstrap.client.ui.FluidContainer;
+import com.github.gwtbootstrap.client.ui.FluidRow;
 import com.github.gwtbootstrap.client.ui.ProgressBar;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiHandler;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -20,9 +27,11 @@ public class BooksViewImpl extends Composite implements BooksView {
     }
 
     @UiField
-    ProgressBar progressBar;
+    ProgressBar    progressBar;
     @UiField
-    Button      searchBtn;
+    Button         searchBtn;
+    @UiField
+    FluidContainer readonlyGrid;
 
     public BooksViewImpl() {
         initWidget(uiBinder.createAndBindUi(this));
@@ -32,6 +41,61 @@ public class BooksViewImpl extends Composite implements BooksView {
 
     @UiHandler("searchBtn")
     public void clickSearch(final ClickEvent e) {
+        readonlyGrid.clear();
         progressBar.setVisible(true);
+
+        new Timer() {
+
+            @Override
+            public void run() {
+                final ArrayList<Book> books = new ArrayList<Book>();
+                for (int i = 0; i < 5; i++) {
+                    final Book book = new Book() //
+                            .title("title " + i) //
+                            .author("author " + i) //
+                            .editor("editor " + i) //
+                            .category("cat " + i) //
+                            .year(1980 + i) //
+                            .comment("comment " + i) //
+                            .id(1L * i) //
+                    ;
+                    books.add(book);
+                }
+                setBooks(books);
+            }
+
+        }.schedule(750);
     }
+
+    private void setBooks(final ArrayList<Book> books) {
+        progressBar.setVisible(false);
+
+        for (final Book book : books) {
+
+            final Column titleCol = new Column(2);
+            final Column authorCol = new Column(2);
+            final Column editorCol = new Column(2);
+            final Column categoryCol = new Column(2);
+            final Column yearCol = new Column(2);
+            final Column commentCol = new Column(2);
+
+            titleCol.getElement().setInnerText(book.getTitle());
+            authorCol.getElement().setInnerText(book.getAuthor());
+            editorCol.getElement().setInnerText(book.getEditor());
+            categoryCol.getElement().setInnerText(book.getCategory());
+            yearCol.getElement().setInnerText("" + book.getYear());
+            commentCol.getElement().setInnerText(book.getComment());
+
+            final FluidRow row = new FluidRow();
+            row.add(titleCol);
+            row.add(authorCol);
+            row.add(editorCol);
+            row.add(categoryCol);
+            row.add(yearCol);
+            row.add(commentCol);
+
+            readonlyGrid.add(row);
+        }
+    }
+
 }
