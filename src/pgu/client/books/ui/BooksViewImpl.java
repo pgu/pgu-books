b2bd35fdf86d76065a2925e41c991a2121e4d486
Guilcher Pascal <pgu.dev@gmail.com>
2012-07-12 20:34:38 +0200

    ..

--- src/pgu/client/books/ui/BooksViewImpl.java
+++ src/pgu/client/books/ui/BooksViewImpl.java
@@ -2,8 +2,13 @@ package pgu.client.books.ui;
 
 import pgu.client.books.BooksView;
 
+import com.github.gwtbootstrap.client.ui.Button;
+import com.github.gwtbootstrap.client.ui.ProgressBar;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -14,8 +19,19 @@ public class BooksViewImpl extends Composite implements BooksView {
     interface BooksViewImplUiBinder extends UiBinder<Widget, BooksViewImpl> {
     }
 
+    @UiField
+    ProgressBar progressBar;
+    @UiField
+    Button      searchBtn;
+
     public BooksViewImpl() {
         initWidget(uiBinder.createAndBindUi(this));
+        progressBar.setVisible(false);
+
     }
 
+    @UiHandler("searchBtn")
+    public void clickSearch(final ClickEvent e) {
+        progressBar.setVisible(true);
+    }
 }
