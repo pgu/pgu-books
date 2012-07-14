ae50264d06a2e8793138f67cf549498288a24587
Guilcher Pascal <pgu.dev@gmail.com>
2012-07-14 18:21:20 +0200

    ..

--- src/pgu/client/importBooks/ui/ImportBooksViewImpl.java
+++ src/pgu/client/importBooks/ui/ImportBooksViewImpl.java
@@ -2,8 +2,10 @@ package pgu.client.importBooks.ui;
 
 import pgu.client.importBooks.ImportBooksView;
 
+import com.github.gwtbootstrap.client.ui.base.TextBox;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 
@@ -13,6 +15,9 @@ public class ImportBooksViewImpl extends Composite implements ImportBooksView {
 
     interface ImportBooksViewImplUiBinder extends UiBinder<Widget, ImportBooksViewImpl> {
     }
+
+    @UiField
+    TextBox startBox, lengthBox;
 
     public ImportBooksViewImpl() {
         initWidget(uiBinder.createAndBindUi(this));
